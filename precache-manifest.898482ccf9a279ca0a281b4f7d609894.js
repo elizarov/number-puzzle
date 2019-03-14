849030dfe9960c16dd5261ef66e2621df1208f82
Roman Elizarov <elizarov@gmail.com>
2019-03-14 18:42:05 +0300

    Updates

---
+++ precache-manifest.898482ccf9a279ca0a281b4f7d609894.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2bf08cfc8401573e5fb",
+    "url": "/number-puzzle/static/css/main.ee168e4e.chunk.css"
+  },
+  {
+    "revision": "a2bf08cfc8401573e5fb",
+    "url": "/number-puzzle/static/js/main.9d1528ca.chunk.js"
+  },
+  {
+    "revision": "6dda2868e4d4a09608e4",
+    "url": "/number-puzzle/static/js/runtime~main.e81a9b28.js"
+  },
+  {
+    "revision": "a6be77995ce550f29dab",
+    "url": "/number-puzzle/static/js/2.07dbee22.chunk.js"
+  },
+  {
+    "revision": "a7525d5730eaba0e869861b2f5e8d733",
+    "url": "/number-puzzle/index.html"
+  }
+];
